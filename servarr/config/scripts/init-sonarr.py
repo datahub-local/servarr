522bead35794ac6375b64ba7a728510616ebed61
Alvaro Santos Andres <alvsanand@gmail.com>
2026-01-05 21:28:35 +0100

    refactor: make init configuration more robust

--- servarr/config/scripts/init-sonarr.py
+++ servarr/config/scripts/init-sonarr.py
@@ -1,16 +1,7 @@
 #!/usr/local/bin/python3
 
-import requests
-import os, sys
-import logging
-from json import JSONDecodeError
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-console_handler = logging.StreamHandler()
-log_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-console_handler.setFormatter(log_format)
-logger.addHandler(console_handler)
+import os
+from utils import post, step, logger
 
 SONARR_HOST = os.getenv("SONARR_HOST")
 API_KEY = os.getenv("API_KEY")
@@ -18,199 +9,138 @@ TORRENT_USERNAME = os.getenv("TORRENT_ADMIN")
 TORRENT_PASSWORD = os.getenv("TORRENT_PASSWORD")
 TORRENT_SERVICE = os.getenv("TORRENT_SERVICE")
 
-def post(url: str, headers: dict, body: dict):
-    """
-    Handle POST requests towards an url, logging both
-    the details before sending it and the response.
 
-    Parameters
-    ----------
-    url : str
-        HTTP request URL as string
-    headers : dict
-        HTTP headers to be used in the request
-    body : dict
-        Request body needed for the POST
-    """
+@step("sonarr_qbittorrent")
+def setup_qbittorrent():
+    logger.info("Setup Sonarr and qBitTorrent interworking")
 
-    logger.debug(" ".join([
-        "POST",
-        url,
-        ", ".join(f'{key}: {value}' for key,value in headers.items()),
-        str(body)
-    ]))
+    headers = {
+        "content-type": "application/json",
+        "x-api-key": API_KEY,
+        "x-requested-with": "XMLHttpRequest",
+    }
 
-    response = requests.post(
-        url=url,
-        json=body,
-        headers=headers
+    body = {
+        "enable": True,
+        "protocol": "torrent",
+        "priority": 1,
+        "removeCompletedDownloads": True,
+        "removeFailedDownloads": True,
+        "name": "qBittorrent",
+        "fields": [
+            {"name": "host", "value": TORRENT_SERVICE},
+            {"name": "port", "value": "10095"},
+            {"name": "useSsl", "value": False},
+            {"name": "urlBase"},
+            {"name": "username", "value": TORRENT_USERNAME},
+            {"name": "password", "value": TORRENT_PASSWORD},
+            {"name": "movieCategory", "value": "radarr"},
+            {"name": "movieImportedCategory"},
+            {"name": "recentMoviePriority", "value": 0},
+            {"name": "olderMoviePriority", "value": 0},
+            {"name": "initialState", "value": 0},
+            {"name": "sequentialOrder", "value": False},
+            {"name": "firstAndLast", "value": False},
+            {"name": "contentLayout", "value": 0},
+        ],
+        "implementationName": "qBittorrent",
+        "implementation": "QBittorrent",
+        "configContract": "QBittorrentSettings",
+        "infoLink": "https://wiki.servarr.com/radarr/supported#qbittorrent",
+        "tags": [],
+    }
+
+    post(
+        url="http://{}/api/v3/downloadclient".format(SONARR_HOST),
+        headers=headers,
+        body=body,
     )
 
-    logger.debug(" ".join([
-        "Status Code:",
-        str(response.status_code),
-        "Response body:",
-        response.text
-    ]))
 
-    try:
-        return {"code": response.status_code, "response": response.json()}
-    except JSONDecodeError:
-        return {"code": response.status_code, "response": response.text}
+@step("sonarr_remote_path_mapping")
+def setup_remote_path_mapping():
+    logger.info("Setup qBitTorrent Remote Path Mapping in Sonarr")
 
-logger.info("Setup Sonarr and qBitTorrent interworking")
+    headers = {
+        "content-type": "application/json",
+        "x-api-key": API_KEY,
+        "x-requested-with": "XMLHttpRequest",
+    }
 
-headers = {
-    "content-type": "application/json",
-    "x-api-key": API_KEY,
-    "x-requested-with": "XMLHttpRequest"
-}
+    body = {
+        "host": TORRENT_SERVICE,
+        "remotePath": "/downloads",
+        "localPath": "/mnt/downloads/",
+    }
 
-body = {
-    "enable": True,
-    "protocol": "torrent",
-    "priority": 1,
-    "removeCompletedDownloads": True,
-    "removeFailedDownloads": True,
-    "name": "qBittorrent",
-    "fields": [
-        {
-            "name": "host",
-            "value": TORRENT_SERVICE
-        },
-        {
-            "name": "port",
-            "value": "10095"
-        },
-        {
-            "name": "useSsl",
-            "value": False
-        },
-        {
-            "name": "urlBase"
-        },
-        {
-            "name": "username",
-            "value": TORRENT_USERNAME
-        },
-        {
-            "name": "password",
-            "value": TORRENT_PASSWORD
-        },
-        {
-            "name": "movieCategory",
-            "value": "radarr"
-        },
-        {
-            "name": "movieImportedCategory"
-        },
-        {
-            "name": "recentMoviePriority",
-            "value": 0
-        },
-        {
-            "name": "olderMoviePriority",
-            "value": 0
-        },
-        {
-            "name": "initialState",
-            "value": 0
-        },
-        {
-            "name": "sequentialOrder",
-            "value": False
-        },
-        {
-            "name": "firstAndLast",
-            "value": False
-        },
-        {
-            "name": "contentLayout",
-            "value": 0
-        }
-    ],
-    "implementationName": "qBittorrent",
-    "implementation": "QBittorrent",
-    "configContract": "QBittorrentSettings",
-    "infoLink": "https://wiki.servarr.com/radarr/supported#qbittorrent",
-    "tags": []
-}
+    post(
+        url="http://{}/api/v3/remotepathmapping".format(SONARR_HOST),
+        headers=headers,
+        body=body,
+    )
 
-res = post(
-    url="http://{}/api/v3/downloadclient".format(SONARR_HOST),
-    headers=headers,
-    body=body
-)
 
-if res["code"] != 201:
-    logger.error("There was an error while setting qBitTorrent in Sonarr!")
-    sys.exit(1)
+@step("sonarr_root_folder")
+def setup_root_folder():
+    logger.info("Setup Root Folder in Sonarr")
 
-logger.info("Setup qBitTorrent Remote Path Mapping in Sonarr")
+    headers = {
+        "content-type": "application/json",
+        "x-api-key": API_KEY,
+        "x-requested-with": "XMLHttpRequest",
+    }
 
-body = {
-    "host": TORRENT_SERVICE,
-    "remotePath": "/downloads",
-    "localPath": "/mnt/downloads/"
-}
+    body = {"path": "/mnt/media/"}
 
-res = post(
-    url="http://{}/api/v3/remotepathmapping".format(SONARR_HOST),
-    headers=headers,
-    body=body
-)
+    post(
+        url="http://{}/api/v3/rootFolder".format(SONARR_HOST),
+        headers=headers,
+        body=body,
+    )
 
-if res["code"] != 201:
-    logger.error("There was an error while setting the Remote Path Mapping!")
-    sys.exit(1)
 
-logger.info("Setup Root Folder in Sonarr")
+@step("sonarr_media_management")
+def setup_media_management():
+    headers = {
+        "content-type": "application/json",
+        "x-api-key": API_KEY,
+        "x-requested-with": "XMLHttpRequest",
+    }
 
-body = {
-    "path":"/mnt/media/"
-}
+    body = {
+        "autoUnmonitorPreviouslyDownloadedMovies": False,
+        "recycleBin": "",
+        "recycleBinCleanupDays": 7,
+        "downloadPropersAndRepacks": "preferAndUpgrade",
+        "createEmptyMovieFolders": False,
+        "deleteEmptyFolders": False,
+        "fileDate": "none",
+        "rescanAfterRefresh": "always",
+        "autoRenameFolders": False,
+        "pathsDefaultStatic": False,
+        "setPermissionsLinux": False,
+        "chmodFolder": "755",
+        "chownGroup": "",
+        "skipFreeSpaceCheckWhenImporting": False,
+        "minimumFreeSpaceWhenImporting": 100,
+        "copyUsingHardlinks": False,
+        "useScriptImport": False,
+        "scriptImportPath": "",
+        "importExtraFiles": False,
+        "extraFileExtensions": "srt",
+        "enableMediaInfo": True,
+        "id": 1,
+    }
 
-res = post(
-    url="http://{}/api/v3/rootFolder".format(SONARR_HOST),
-    headers=headers,
-    body=body
-)
+    post(
+        url="http://{}/api/v3/config/mediamanagement".format(SONARR_HOST),
+        headers=headers,
+        body=body,
+        method="PUT",
+    )
 
-if res["code"] != 201:
-    logger.error("There was an error while setting the Root Folder!")
-    sys.exit(1)
 
-body = {
-    "autoUnmonitorPreviouslyDownloadedMovies": False,
-    "recycleBin": "",
-    "recycleBinCleanupDays": 7,
-    "downloadPropersAndRepacks": "preferAndUpgrade",
-    "createEmptyMovieFolders": False,
-    "deleteEmptyFolders": False,
-    "fileDate": "none",
-    "rescanAfterRefresh": "always",
-    "autoRenameFolders": False,
-    "pathsDefaultStatic": False,
-    "setPermissionsLinux": False,
-    "chmodFolder": "755",
-    "chownGroup": "",
-    "skipFreeSpaceCheckWhenImporting": False,
-    "minimumFreeSpaceWhenImporting": 100,
-    "copyUsingHardlinks": False,
-    "useScriptImport": False,
-    "scriptImportPath": "",
-    "importExtraFiles": False,
-    "extraFileExtensions": "srt",
-    "enableMediaInfo": True,
-    "id": 1,
-}
-
-res = post(
-    url="http://{}/api/v3/config/mediamanagement".format(SONARR_HOST),
-    headers=headers,
-    body=body,
-)
-
-if res["code"] != 201:
-    logger.error("There was an error while setting the Configure Media Management!")
-    sys.exit(1)
+setup_qbittorrent()
+setup_remote_path_mapping()
+setup_root_folder()
+setup_media_management()
---
+++ servarr/templates/_helpers.tpl
@@ -0,0 +1,81 @@
+{{/*
+Get or Generate API Key
+*/}}
+{{- define "servarr.apiKey" -}}
+{{- $apiKey := . | default "" -}}
+{{- if $apiKey -}}
+{{- if or (lt (len $apiKey) 20) (gt (len $apiKey) 32) -}}
+{{- fail (printf "API Key must be between 20 and 32 characters long, got %d characters" (len $apiKey)) -}}
+{{- end -}}
+{{- if not (regexMatch "^[a-fA-F0-9]+$" $apiKey) -}}
+{{- fail "API Key must be a hexadecimal string" -}}
+{{- end -}}
+{{- $apiKey -}}
+{{- else -}}
+{{- printf "%s-servarr-api-key" .Release.Name | sha256sum | trunc 32 -}}
+{{- end -}}
+{{- end -}}
+
+{{/*
+Expand the name of the chart.
+*/}}
+{{- define "servarr.name" -}}
+{{- default .Chart.Name .Values.nameOverride | trunc 63 | trimSuffix "-" }}
+{{- end }}
+
+{{/*
+Create a default fully qualified app name.
+We truncate at 63 chars because some Kubernetes name fields are limited to this (by the DNS naming spec).
+If release name contains chart name it will be used as a full name.
+*/}}
+{{- define "servarr.fullname" -}}
+{{- if .Values.fullnameOverride }}
+{{- .Values.fullnameOverride | trunc 63 | trimSuffix "-" }}
+{{- else }}
+{{- $name := default .Chart.Name .Values.nameOverride }}
+{{- if contains $name .Release.Name }}
+{{- .Release.Name | trunc 63 | trimSuffix "-" }}
+{{- else }}
+{{- printf "%s-%s" .Release.Name $name | trunc 63 | trimSuffix "-" }}
+{{- end }}
+{{- end }}
+{{- end }}
+
+{{/*
+Create chart name and version as used by the chart label.
+*/}}
+{{- define "servarr.chart" -}}
+{{- printf "%s-%s" .Chart.Name .Chart.Version | replace "+" "_" | trunc 63 | trimSuffix "-" }}
+{{- end }}
+
+{{/*
+Common labels
+*/}}
+{{- define "servarr.labels" -}}
+helm.sh/chart: {{ include "servarr.chart" . }}
+{{ include "servarr.selectorLabels" . }}
+{{- if .Chart.AppVersion }}
+app.kubernetes.io/version: {{ .Chart.AppVersion | quote }}
+{{- end }}
+app.kubernetes.io/managed-by: {{ .Release.Service }}
+{{- with .Values.global.labels }}
+{{ toYaml . }}
+{{- end }}
+{{- end }}
+
+{{/*
+Selector labels
+*/}}
+{{- define "servarr.selectorLabels" -}}
+app.kubernetes.io/name: {{ include "servarr.name" . }}
+app.kubernetes.io/instance: {{ .Release.Name }}
+{{- end }}
+
+{{/*
+Common annotations
+*/}}
+{{- define "servarr.annotations" -}}
+{{- with .Values.global.annotations }}
+{{ toYaml . }}
+{{- end }}
+{{- end }}
