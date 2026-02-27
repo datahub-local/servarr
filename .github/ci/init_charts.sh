95a1c1c4f0070732a1eaf5176901123dc9697d73
Alvaro Santos Andres <alvsanand@gmail.com>
2026-02-27 18:54:51 +0100

    feat(auralarchive): add logs persistence configuration

--- .github/ci/init_charts.sh
+++ .github/ci/init_charts.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+helm repo add datahub-local https://datahub-local.github.io/servarr/
+
 for chart in charts/*/; do
   if [ -f "$chart/Chart.yaml" ]; then
     helm dependency update "$chart"
